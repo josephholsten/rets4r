5dfb3db536a682b5f881be6190712b3d7f43b811
bgetting <brian@terra-firma-design.com>
2009-08-02 20:30:08 -0700

    Initial commit to rets4r.

---
+++ test/rets4r_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Rets4rTest < Test::Unit::TestCase
+  should "probably rename this file and start testing for real" do
+    flunk "hey buddy, you should probably rename this file and start testing for real"
+  end
+end
