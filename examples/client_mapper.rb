f6376ea7ff5b35c88636ff6d534a1f5dff9f4fc5
Joseph Anthony Pasquale Holsten <joseph@josephholsten.com>
2010-07-02 11:43:38 -0500

    add a mapper example

---
+++ examples/client_mapper.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+$:.unshift 'lib'
+require 'rets4r'
+require 'yaml'
+
+listing_service_config_file = File.expand_path(File.join(File.dirname(__FILE__), "settings.yml"))
+RETS4R::ListingService.configurations = YAML.load_file(listing_service_config_file)
+RETS4R::ListingService.env = ENV['RETS4RENV'] || 'development'
+
+xml = ARGF
+
+mapper = RETS4R::ListingMapper.new
+RETS4R::Loader.load(xml) do |record|
+  attributes = mapper.map(record)
+  puts attributes.inspect
+end
