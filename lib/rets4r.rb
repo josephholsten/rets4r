1b1986f2483b30cd63d2e4039e7a3f09e90d9c51
Joseph Anthony Pasquale Holsten <joseph@josephholsten.com>
2011-05-13 02:03:01 -0500

    use normal require path

--- lib/rets4r.rb
+++ lib/rets4r.rb
@@ -1,14 +1,10 @@
-# Add lib/rets4r as a default load path.
-dir = File.join File.dirname(__FILE__), 'rets4r'
-$:.unshift(dir) unless $:.include?(dir) || $:.include?(File.expand_path(dir))
-
 module RETS4R # :nodoc:
   VERSION = '1.1.18'
 end
 
-require 'client'
-require 'loader'
-require 'client/parsers/compact_nokogiri'
+require 'rets4r/client'
+require 'rets4r/loader'
+require 'rets4r/client/parsers/compact_nokogiri'
 require 'rets4r/listing_service'
 require 'rets4r/listing_mapper'
 require 'rets4r/response_document'
