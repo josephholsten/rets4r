9b3c63309c6b0f6137aea162c2e732310b69fcb4
Joseph Anthony Pasquale Holsten <joseph@josephholsten.com>
2011-05-13 01:37:12 -0500

    stub http request

--- test/test_requester.rb
+++ test/test_requester.rb
@@ -4,7 +4,7 @@ require 'test_helper'
 class TestRequester < Test::Unit::TestCase
   context RETS4R::Client::Requester do
     setup do
-      @uri = URI.parse('http://demo.crt.realtors.org:6103/rets/login')
+      @uri = URI.parse('http://rets.example:6103/rets/login')
       @requester = RETS4R::Client::Requester.new
     end
     context "when the last response contains a RETS-Session-ID cookie" do
@@ -15,7 +15,8 @@ class TestRequester < Test::Unit::TestCase
         BODY
         @response = stub('HTTPResponse', :code => '200', :body => body)
         @response.stubs(:get_fields).with('set-cookie').returns(['RETS-Session-ID=2qwiti55hq311j553ihivc3r; path=/'])
-        Net::HTTP.any_instance.stubs(:get => @response )
+        @http = stub('HTTP', :get => @response )
+        Net::HTTP.any_instance.stubs(:start).yields(@http)
         @requester.request(@uri)
       end
       context "the next request" do
@@ -23,13 +24,13 @@ class TestRequester < Test::Unit::TestCase
           @requester.request(@uri)
         end
         before_should "not set a RETS-Session-ID header" do
-          Net::HTTP.any_instance.expects(:get).with do |anything,headers|
+          @http.expects(:get).with do |anything,headers|
             assert_does_not_contain headers.keys, 'RETS-Session-ID'
             true
           end.returns(@response)
         end
         before_should "set a RETS-Session cookie" do
-          Net::HTTP.any_instance.expects(:get).with(anything,has_entry('Cookie' => 'RETS-Session-ID=2qwiti55hq311j553ihivc3r')).returns(@response)
+          @http.expects(:get).with(anything,has_entry('Cookie' => 'RETS-Session-ID=2qwiti55hq311j553ihivc3r')).returns(@response)
         end
       end
     end
