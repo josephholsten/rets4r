0d69c86af09bb150bc769fe8fd15d4475d4465c0
Josh Schairbaum <joshua.schairbaum@gmail.com>
2011-05-23 20:37:40 -0500

    re-adding a require for 'rets4r/auth', this was causing errors on 1.9.2 for me. adding the line fixed it

--- lib/rets4r.rb
+++ lib/rets4r.rb
@@ -2,6 +2,7 @@ module RETS4R # :nodoc:
   VERSION = '1.1.18'
 end
 
+require 'rets4r/auth'
 require 'rets4r/client'
 require 'rets4r/client/parsers/compact_nokogiri'
 require 'rets4r/listing_mapper'
