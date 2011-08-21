595a6fa9e5ba9d54e5f02d24f9c13625f9f04d88
Joseph Anthony Pasquale Holsten <joseph@josephholsten.com>
2011-08-21 12:41:23 -0500

    Clean up auth code
    * lib/rets4r/auth.rb (RETS4R::Auth): refactor to use a ResponseHeader object

--- lib/rets4r/auth.rb
+++ lib/rets4r/auth.rb
@@ -13,7 +13,7 @@ module RETS4R
     def update(uri, method, requestId)
       self.uri = uri
       self.method = method
-      self.request_id = requestId || Auth.request_id
+      self.request_id = requestId || self.class.request_id
       self.nc += 1
     end
 
@@ -21,33 +21,31 @@ module RETS4R
       if response['www-authenticate'].nil? || response['www-authenticate'].empty?
         self.current_strategy = :none
         warn "Missing required header 'www-authenticate'. Got: #{response}"
-      elsif response['www-authenticate'] =~ /Basic/
-        self.current_strategy = :basic
       else
-        self.current_strategy = :digest
-        # use Digest Authentication
-        authHeader = Auth.parse_header(response['www-authenticate'])
-        self.realm = authHeader['realm']
-        self.nonce = authHeader['nonce']
-        self.qop = authHeader['qop'] || false
-        self.opaque = authHeader['opaque']
+        header = Auth::ResponseHeader.parse(response['www-authenticate'])
+        self.current_strategy = header.strategy
+        self.realm = header.realm
+        self.nonce = header.nonce
+        self.qop = header.qop
+        self.opaque = header.opaque
       end
+      self
     end
 
     def to_s
-      case self.current_strategy
+      case current_strategy
       when :none
         nil
       when :basic
-        self.to_basic_header
+        to_basic_header
       when :digest
-        self.to_digest_header
+        to_digest_header
       end
     end
 
     def self.authenticate(response, username, password, uri, method, requestId, useragent, nc = 0) # :nodoc:
       warn "#{caller.first}: warning: #{self.class}::authenticate is deprecated and will be removed by rets4r 2.0; use a RETS4R::Auth object instead"
-      auth = RETS4R::Auth.new.tap do |a|
+      auth = new.tap do |a|
         a.username = username
         a.password = password
         a.uri = uri
@@ -80,12 +78,35 @@ module RETS4R
       Digest::MD5.hexdigest("#{useragent}:#{password}:#{requestId}:#{nonce}")
     end
 
-    def self.hA2(method, uri)
+    def self.parse_header(header) # :nodoc:
+      warn "#{caller.first}: warning: #{self.class}::parse_header is deprecated and will be removed by rets4r 2.0; use RETS4R::Auth::ResponseHeader.parse instead"
+      RETS4R::Auth::ResponseHeader.parse(header).to_h
+    end
+
+    def self.hA2(method, uri) # :nodoc:
       Digest::MD5.hexdigest("#{method}:#{uri}")
     end
 
-    def self.hA1(username, realm, password)
+    def self.hA1(username, realm, password) # :nodoc:
       Digest::MD5.hexdigest("#{username}:#{realm}:#{password}")
+    end
+
+    def self.request_id # :nodoc:
+      Digest::MD5.hexdigest(Time.new.to_f.to_s)
+    end
+
+    private
+
+    def response
+      text = [hA1, nonce]
+      if (qop)
+        throw ArgumentException, 'qop requires a cnonce to be provided.' unless cnonce
+
+        text << '%08x' % nc << cnonce << qop
+      end
+      text << hA2
+
+      return Digest::MD5.hexdigest text.join(':')
     end
 
     def to_basic_header
@@ -110,16 +131,6 @@ module RETS4R
       Digest::MD5.hexdigest("#{useragent}:#{password}:#{request_id}:#{nonce}")
     end
 
-    def response
-      if (qop)
-        throw ArgumentException, 'qop requires a cnonce to be provided.' unless cnonce
-
-        response = Digest::MD5.hexdigest("#{hA1}:#{nonce}:#{('%08x' % nc)}:#{cnonce}:#{qop}:#{hA2}")
-      else
-        response = Digest::MD5.hexdigest("#{hA1}:#{nonce}:#{hA2}")
-      end
-    end
-
     def hA1
       Digest::MD5.hexdigest("#{username}:#{realm}:#{password}")
     end
@@ -128,23 +139,41 @@ module RETS4R
       Digest::MD5.hexdigest("#{method}:#{uri}")
     end
 
-    def Auth.request_id
-      Digest::MD5.hexdigest(Time.new.to_f.to_s)
-    end
+    class ResponseHeader
+      ACCEPTABLE_KEYS = %w[realm domain nonce opaque stale algorithm qop]
+      attr_accessor :strategy, *ACCEPTABLE_KEYS
 
-    def Auth.parse_header(header)
-      type = header[0, header.index(' ')]
-      args = header[header.index(' '), header.length].strip.split(',')
+      def self.parse(header)
+        new.tap do |auth|
+          auth.strategy = header[0, header.index(' ')].downcase.to_sym
+          args = header[header.index(' '), header.length].strip.split(',')
 
-      parts = {'type' => type}
-
-      args.each do |arg|
-        name, value = arg.split('=')
-
-        parts[name.downcase.strip] = value.tr('"', '').strip
+          args.each do |arg|
+            key, value = arg.split('=')
+            key = key.downcase.strip
+            value = value.tr('"', '').strip
+            if ACCEPTABLE_KEYS.include? key
+              auth.send("#{key}=".to_sym, value)
+            else
+              warn "#{caller.first}: warning: response header contained unknown value #{key}: \"#{value}\", skipping"
+            end
+          end
+        end
       end
 
-      return parts
+      # convert the header to a hash. Uses strings for keys, with 'type containing the 
+      # Examples:
+      #   header = 'Digest qop="auth", realm="REALM", nonce="2006-03-03T17:37:10", opaque="", stale="false", domain="test-domain"'
+      #   ResponseHeader.parse(header).to_h
+      #   => { "type" => "digest", "qop" => "auth", "realm" => "REALM", "nonce" => "2006-03-03T17:37:10", "opaque" => "", "stale" => "false", "domain" => "test-domain"}
+      def to_h
+        result = {}
+        result['type'] = self.strategy
+        ACCEPTABLE_KEYS.each do |key|
+          result[key] = self.send(key)
+        end
+        result
+      end
     end
   end
 end
